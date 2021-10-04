a62913d9d3c45510246fd4a9109607b7e3858a15
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-10-04 16:18:43 +0800

    Update HelloController.java

--- spring-mvc-02-annotation/controller/HelloController.java
+++ spring-mvc-02-annotation/controller/HelloController.java
@@ -9,23 +9,40 @@ import org.springframework.ui.Model;
  * @date 2021/10/3.20点27分
  */
 
+/**
+ * 用于声明Spring类的实例是一个控制器  表示该类被Spring托管
+ * 类中的方法返回值为String,可以进行跳转页面
+ */
 @Controller
+
+/**
+ * RequestMapping  用于映射url到控制器类或一个特定的处理程序方法
+ * 可用于类或方法上
+ * 用于类上，表示类中的所有响应请求的方法都是以该地址作为父路径
+ * 方法上,请求路径
+ */
 @RequestMapping("/helloController")
+
+
 public class HelloController {
 
     //相当于一个Servlet
     @RequestMapping("/annotation")
     public String annotation(Model model) {
+
         //向模型中添加属性msg 在jsp页面进行渲染
         model.addAttribute("msg", "hello,SpringMVC");
-        //jap.annotation.jsp  视图解析器
-        return "annotation";     //需要跳转的页面
+
+        return "annotation"; //jap.annotation.jsp  视图解析器
     }
-  
-  //@RequestMapping    可以加再类上  也可以加在方法上  
-  
 
 }
+
+
+
+
+
+
 
 
 
