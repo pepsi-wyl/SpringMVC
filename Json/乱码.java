53c1bd1b982472ce6d3e0d364ffe81b85173cf6d
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-10-07 21:41:24 +0800

    Create 乱码.java

---
+++ Json/乱码.java
@@ -0,0 +1,27 @@
+
+
+
+                        RequestMapping中配置
+  @RequestMapping( produces = "application/json;charset=utf-8")
+ 
+   或者
+ 
+                       springmvc-servlet.xml中配置
+  <mvc:annotation-driven>
+        <mvc:message-converters register-defaults="true">
+            <bean class="org.springframework.http.converter.StringHttpMessageConverter">
+                <constructor-arg value="UTF-8"/>
+            </bean>
+            <bean class="org.springframework.http.converter.json.MappingJackson2HttpMessageConverter">
+                <property name="objectMapper">
+                    <bean class="org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean">
+                        <property name="failOnEmptyBeans" value="false"/>
+                    </bean>
+                </property>
+            </bean>
+        </mvc:message-converters>
+    </mvc:annotation-driven>
+    
+    
+    
+ 
