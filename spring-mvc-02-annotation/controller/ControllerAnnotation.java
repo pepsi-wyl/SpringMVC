921132d2612d9eddc2cdff70f707db1fc7bdacc5
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-08 15:39:08 +0800

    Update and rename HelloController.java to ControllerAnnotation.java

--- spring-mvc-02-annotation/controller/ControllerAnnotation.java
+++ spring-mvc-02-annotation/controller/ControllerAnnotation.java
@@ -1,8 +1,8 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author by wyl
@@ -10,10 +10,12 @@ import org.springframework.ui.Model;
  */
 
 /**
- * 用于声明Spring类的实例是一个控制器  表示该类被Spring托管
- * 类中的方法返回值为String,可以进行跳转页面
+ * @Controller
+ * 表示该类被Spring托管 用于声明Spring类的实例是一个控制器
+ * 类中的方法返回值为String,可以进行跳转页面(被视图解析器解析)
  */
-@Controller
+@Controller(value = "tController")
+
 
 /**
  * RequestMapping  用于映射url到控制器类或一个特定的处理程序方法
@@ -21,10 +23,9 @@ import org.springframework.ui.Model;
  * 用于类上，表示类中的所有响应请求的方法都是以该地址作为父路径
  * 方法上,请求路径
  */
-@RequestMapping("/helloController")
+@RequestMapping("/controller")
 
-
-public class HelloController {
+public class T_Controller {
 
     //相当于一个Servlet
     @RequestMapping("/annotation")
@@ -33,11 +34,10 @@ public class HelloController {
         //向模型中添加属性msg 在jsp页面进行渲染
         model.addAttribute("msg", "hello,SpringMVC");
 
-        return "annotation"; //jap.annotation.jsp  视图解析器
+        return "WEB-INF/jsp/annotation";   // WEB-INF/jap/annotation.jsp  视图解析器
     }
 
 }
-
 
 
 
