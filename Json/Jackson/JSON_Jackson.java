9103bab76b8f592d46ff02b5e74c363819c685eb
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-09 17:00:38 +0800

    Update JSON_Jackson.java

--- Json/Jackson/JSON_Jackson.java
+++ Json/Jackson/JSON_Jackson.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pojo.User;
-import utils.JsonUtils;
+import utils.JacksonUtils;
 
 import java.util.*;
 
@@ -14,16 +14,19 @@ import java.util.*;
  * @author by wyl
  * @date 2021/10/5.19点45分
  * JSON_Jackson 测试类
+ */
+
+/**
  * @Controller +  @ResponseBody 方法返回JSON字符串
  * @RestController 直接使用该注解，类的方法全部返回JSON字符串
  */
 
 /**
- produces = "application/json;charset=utf-8"    设置中文相应乱码相应     在MVC配置直接配置(通用配置)
+ produces = "application/json;charset=utf-8"    设置中文响应乱码返回数据为Json格式    在MVC配置直接配置(通用配置)
  @RequestMapping(value = "/XXX", method = RequestMethod.XXX ,produces = "application/json;charset=utf-8")
  */
 
-@Controller
+@Controller(value = "JSON_Jackson")
 
 @RequestMapping("/JSON_Jackson")
 public class JSON_Jackson {
@@ -31,19 +34,19 @@ public class JSON_Jackson {
     /**
      * 解析对象
      */
-    @RequestMapping(value = "/test1", method = RequestMethod.GET)
+    @RequestMapping(value = "/test1", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     @ResponseBody            //返回JSON字符串
     @SneakyThrows            //lombok注解抛异常
     public String test1() {
         User user = new User(1, "武扬岚", 20);
-        String str = JsonUtils.toJson(user);
-        User user1 = JsonUtils.jsonToObj(str, User.class);
+        String str = JacksonUtils.toJson(user);
+        User user1 = JacksonUtils.jsonToObj(str, User.class);
         System.out.println(user1);
-        return JsonUtils.toJson(user);
+        return JacksonUtils.toJson(user);
     }
 
     /**
-     * 解析集合
+     * 解析List
      */
     @RequestMapping(value = "/test2", method = RequestMethod.GET)
     @ResponseBody            //返回JSON字符串
@@ -54,14 +57,14 @@ public class JSON_Jackson {
         list.add(new User(2, "武扬岚", 20));
         list.add(new User(3, "武扬岚", 20));
         list.add(new User(4, "武扬岚", 20));
-        String str = JsonUtils.toJson(list);
-        List<User> users = JsonUtils.jsonToList(str, User.class);
+        String str = JacksonUtils.toJson(list);
+        List<User> users = JacksonUtils.jsonToList(str, User.class);
         users.forEach((v) -> System.out.println(v));
-        return JsonUtils.toJson(list);
+        return JacksonUtils.toJson(list);
     }
 
     /**
-     * 解析时间
+     * 解析Map
      */
     @RequestMapping(value = "/test3", method = RequestMethod.GET)
     @ResponseBody              //返回JSON字符串
@@ -70,11 +73,11 @@ public class JSON_Jackson {
         HashMap<Integer, User> personMap = new HashMap<>();
         personMap.put(1, new User(1, "武扬岚", 20));
         personMap.put(2, new User(1, "武扬岚", 20));
-        String str = JsonUtils.toJson(personMap);
+        String str = JacksonUtils.toJson(personMap);
         System.out.println(str);
-        Map<Integer, User> map = JsonUtils.jsonToMap(str, Integer.class, User.class);
+        Map<Integer, User> map = JacksonUtils.jsonToMap(str, Integer.class, User.class);
         map.forEach((k, v) -> System.out.println(k + " " + v));
-        return JsonUtils.toJson(personMap);
+        return JacksonUtils.toJson(personMap);
     }
 
     /**
@@ -84,11 +87,11 @@ public class JSON_Jackson {
     @ResponseBody              //返回JSON字符串
     @SneakyThrows              //lombok注解抛异常
     public String test4() {
-        String str = JsonUtils.toJson(new Date());
+        String str = JacksonUtils.toJson(new Date());
         System.out.println(str);
-        Date date = JsonUtils.jsonToObj(str, Date.class);
+        Date date = JacksonUtils.jsonToObj(str, Date.class);
         System.out.println(date);
-        return JsonUtils.toJson(new Date());
+        return JacksonUtils.toJson(new Date());
     }
 
 
